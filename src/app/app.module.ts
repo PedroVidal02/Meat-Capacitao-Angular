3505b47b6251d0fd05abf66bfae1c6c9235ba780
PedroVidal02 <pedrovidal2703@gmail.com>
2021-01-18 18:13:09 -0300

    made input.component for form validation and passed validation from order.component to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,8 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component'
+import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component'
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { OrderComponent } from './order/order.component'
     ShoppingCartComponent,
     ReviewsComponent,
     OrderComponent,
+    InputComponent,
   ],
   imports: [
     BrowserModule,
