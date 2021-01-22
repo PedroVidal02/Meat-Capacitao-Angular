6a779c19ec7813d549e5f8e766165b7c639d3b67
PedroVidal02 <pedrovidal2703@gmail.com>
2021-01-21 23:13:35 -0300

    added new component order-items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component'
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component'
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { RadioComponent } from './shared/radio/radio.component'
     OrderComponent,
     InputComponent,
     RadioComponent,
+    OrderItemsComponent,
   ],
   imports: [
     BrowserModule,
