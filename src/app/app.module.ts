12231882fc1052a4e1271a4c705b1fba6c704880
PedroVidal02 <pedrovidal2703@gmail.com>
2021-01-24 12:07:13 -0300

    generated component order-summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,8 @@ import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component'
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component'
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component'
 
 @NgModule({
   declarations: [
@@ -45,6 +46,7 @@ import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.co
     RadioComponent,
     OrderItemsComponent,
     DeliveryCostsComponent,
+    OrderSummaryComponent,
   ],
   imports: [
     BrowserModule,
