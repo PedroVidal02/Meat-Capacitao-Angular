8a270bb845954b643fbc1cca140f26a281ae6d31
PedroVidal02 <pedrovidal2703@gmail.com>
2021-01-24 12:10:05 -0300

    added a navigation to order-summary after checkOrder

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router'
+
 import { RadioOption } from '../shared/radio/radio-option.model'
 import { OrderService } from './order.service'
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model'
@@ -19,7 +21,7 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão Refeição', value: 'REF' }
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -46,7 +48,7 @@ export class OrderComponent implements OnInit {
     order.orderItems = this.cartItems()
     .map((item:CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order).subscribe( (orderId: string) => {
-      console.log(`Compra concluída: ${orderId}`)
+      this.router.navigate(['/order-summary'])
       this.orderService.clear()
     })
     console.log(order)
